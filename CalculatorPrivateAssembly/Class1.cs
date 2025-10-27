0c3bd0e048caea1d7990fa9c6252abd66e42e96c
esmlheaia <miya.solenee@gmail.com>
2025-10-27 18:55:59 +0800

    CREATE A PRIVATE ASSEMBLY

--- CalculatorPrivateAssembly/Class1.cs
+++ CalculatorPrivateAssembly/Class1.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace CalculatorPrivateAssembly
 {
-    public class Class1
+    public class BasicComputation
     {
-
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        public static float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+        public static float Divide(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
     }
 }
